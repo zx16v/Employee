4cf0832ba406a727041553a2b09a6a3b65cdffef
zx16v <shimi.levi@gmail.com>
2016-12-05 13:53:30 +0200

    Add files via upload

---
+++ BaseLogic.cs
@@ -0,0 +1,21 @@
+﻿using Dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employment
+{
+    public abstract class BaseLogic : IDisposable
+    {
+        protected readonly EmployeeEntities DB = new EmployeeEntities();
+     
+        public void Dispose()
+        {
+            DB.Dispose();
+        }
+
+
+    }
+}
---
+++ EmployeesLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dal;
+namespace employment
+{
+   public  class EmployeesLogic:BaseLogic
+    {
+        public List<GETEmployeeList_Result> GetAllEmployees()
+        {
+
+            return DB.GETEmployeeList().ToList();
+        }
+
+        public List<GetEmployeeRoles_Result> GetEmployeeRoles(int EmployeeId)
+        {
+            return DB.GetEmployeeRoles(EmployeeId).ToList();
+        }
+
+        public void AddAttenance(Attenance attenance)
+        {
+            DB.Attenances.Add(attenance);
+            DB.SaveChanges();
+        }
+
+    }
+}
