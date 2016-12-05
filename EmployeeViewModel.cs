2402c6d79e9c531c23f90f8bf8d36f7c945e6db0
zx16v <shimi.levi@gmail.com>
2016-12-05 13:51:54 +0200

    Add files via upload

---
+++ EmployeeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Employees.Models
+{
+    public class EmployeeViewModel
+    {
+        [Required]
+        [Display(Name = "employeeId")]
+        public int employeeId { get; set; }
+
+        [Required]
+        [Display(Name = "roleId")]
+        public int roleId { get; set; }
+    }
+
+
+}
+
