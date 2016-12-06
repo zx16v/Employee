cb2fa881862b5295917f70121b453cbe1ae42bca
zx16v <shimi.levi@gmail.com>
2016-12-06 08:42:48 +0200

    Update HomeController.cs

--- HomeController.cs
+++ HomeController.cs
@@ -65,18 +65,5 @@ namespace Employees.Controllers
         }
 
 
-        //public ActionResult About()
-        //{
-        //    ViewBag.Message = "Your application description page.";
-
-        //    return View();
-        //}
-
-        //public ActionResult Contact()
-        //{
-        //    ViewBag.Message = "Your contact page.";
-
-        //    return View();
-        //}
-    }
+            }
 }
